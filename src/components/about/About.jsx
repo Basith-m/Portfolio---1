97e81bf87bb2213b1a34ad54c6dc63179c81cf7f
Basith-m <abm2k20@gmail.com>
2024-02-28 12:54:08 +0530

    Asset updated

---
+++ src/components/about/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import aboutImage from '../../assets/about.JPG'
+import CV from '../../assets/'
+import './about.css'
+
+const About = () => {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section_title">
+            About Me
+        </h2>
+        <span className="section_subtitle">
+            My Introduction
+        </span>
+        <div className="about_container container grid">
+
+        </div>
+    </section>
+  )
+}
+
+export default About
